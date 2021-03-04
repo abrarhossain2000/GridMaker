0be2b6dc6ae8ceb739a81d82a75417a7ed5062da
Abrar Hossain <66147832+abrarhossain2000@users.noreply.github.com>
2021-03-04 17:26:39 -0500

    Abrar's Commit 1

---
+++ script.js
@@ -0,0 +1,61 @@
+let numRows = 0;
+let numCols = 0;
+let colorSelected;
+
+//Adds a row
+function addR() 
+{ 
+    var tbl = document.getElementById("grid"); 
+    row = tbl.insertRow(tbl.rows.length); 
+    for (i = 0; i < tbl.rows[0].cells.length; i++) 
+    { 
+        createCell = (row.insertCell(i), i, 'row'); 
+    }
+}
+
+// Adds a column
+function addC() 
+{ 
+    var tbl = document.getElementById('grid'); 
+    for (i = 0; i < tbl.rows.length; i++) 
+    { 
+        createCell= (tbl.rows[i].insertCell(tbl.rows[i].cells.length), i, 'col'); 
+    }
+}
+
+// Removes a row
+function removeR() {
+    // gets first tr tag then removes it
+    var row = document.body.getElementsByTagName("tr")[0];
+    row.remove();
+}
+
+// Removes a column
+function removeC() {
+    // gets every tr tag and stores it in row
+    var row = document.body.getElementsByTagName("tr");
+
+    // removes cell at index 0 in each row
+    var i = 0;  
+    for (var j = 0; j < row.length; j++) { 
+        row[j].deleteCell(i); 
+    }
+}
+
+// sets global var for selected color
+function selected() {
+    colorSelected = document.getElementById("selectedID").value;
+    console.log(colorSelected);
+}
+
+function fill() {
+    alert("Clicked Fill All");
+}
+
+function clearAll() {
+    alert("Clicked Clear All");
+}
+
+function fillU() {
+    alert("Clicked Fill All Uncolored");
+}
